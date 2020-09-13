5fe6964615975469622e0e25c4c6f31154d04c8e
atamyratabdy <aaabdyrahmanov@gmail.com>
2020-09-13 23:51:57 +0300

    Improve codebase of handling dependencies route

--- controller/index.js
+++ controller/index.js
@@ -1,8 +1,33 @@
 const semver = require("semver");
 const bent = require("bent");
+const { dependencies } = require("../package.json");
 const { NODE_VERSIONS_API } = require("../config");
 
 const getJSON = bent("json");
+
+async function getDependencyList(req, res) {
+  try {
+    const dependencyList = [];
+    // parse and turn the dependency list on usable data
+    Object.entries(dependencies).forEach(([key, value], index) => {
+      const item = {};
+      item.index = index + 1;
+      item.name = key;
+      item.version = value;
+      dependencyList.push(item);
+    });
+    await Promise.all(dependencyList);
+    console.info("Imported dependency list successfully");
+
+    res.status(200).render("main", { packages: dependencyList });
+  } catch (error) {
+    res.status(400).send({
+      code: 400,
+      error,
+    });
+    throw new Error(error);
+  }
+}
 
 async function handleVersionSecurity(req, res) {
   const versionList = [];
@@ -81,6 +106,7 @@ async function handleLatestReleases(req, res) {
   }
 }
 module.exports = {
+  getDependencyList,
   handleVersionSecurity,
   handleLatestReleases,
 };
--- routes/dependencies.js
+++ routes/dependencies.js
@@ -1,24 +1,10 @@
 const router = require("express").Router();
-const { dependencies } = require("../package.json");
+const { getDependencyList: getDependencies } = require("../controller");
 
 /**
  * GET /dependencies
  */
 
-router.get("/", async (req, res) => {
-  const dependencyList = [];
-  // parse and turn the dependency list on usable data
-  Object.entries(dependencies).forEach(([key, value], index) => {
-    const item = {};
-    item.index = index + 1;
-    item.name = key;
-    item.version = value;
-    dependencyList.push(item);
-  });
-  await Promise.all(dependencyList);
-  console.info("Imported dependency list successfully");
-
-  res.render("main", { packages: dependencyList });
-});
+router.get("/", getDependencies);
 
 module.exports = router;
