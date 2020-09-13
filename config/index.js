67d3fb676bf79794167ed7382bcbfeb4011b5c88
atamyratabdy <aaabdyrahmanov@gmail.com>
2020-09-13 06:38:06 +0300

    Add API source on sample environment variables

--- config/index.js
+++ config/index.js
@@ -4,9 +4,11 @@ require('dotenv').config()
 const NODE_ENV = process.env.NODE_ENV
 const HOST = process.env.HOST
 const PORT = process.env.PORT
+const NODEJS_VERSIONS_API = process.env.NODEJS_VERSIONS_API
 
 module.exports = {
   NODE_ENV,
   HOST,
-  PORT
+  PORT,
+  NODEJS_VERSIONS_API
 }
