385c93d087777f7f12d66ff5f91f099c89eb22b2
Atamyrat Abdyrahmanov <atamyrat@abdyrahmanov.com>
2021-06-14 07:41:28 +0300

    Improve test-case definitions and change node-v on latest releases

--- test/index.js
+++ test/index.js
@@ -19,20 +19,20 @@ tape("setup", async (t) => {
   t.end();
 });
 
-tape("should get existing required dependencies", async (t) => {
+tape("/dependencies - # should get dependencies", async (t) => {
   const html = (await getBuffer(`${context.origin}/dependencies`)).toString();
   t.plan(3);
 
   t.equal(
     html.includes("express"),
     true,
-    "# Should contain package - ^express"
+    "* should contain package - ^express"
   );
-  t.equal(html.includes("bent"), true, "# Should contain package - ^bent");
-  t.equal(html.includes("hbs"), true, "# Should contain package - ^hbs");
+  t.equal(html.includes("bent"), true, "* should contain package - ^bent");
+  t.equal(html.includes("hbs"), true, "* should contain package - ^hbs");
 });
 
-tape("should get minimum secure versions", async (t) => {
+tape("/minimum-secure - # should get minimum secure versions", async (t) => {
   const getStream = bent(context.origin);
   const stream = await getStream("/minimum-secure");
   t.plan(3);
@@ -42,26 +42,26 @@ tape("should get minimum secure versions", async (t) => {
     (v) => semver.major(v.version) === 0
   )[0];
   const forthVersion = versions.filter((v) => semver.major(v.version) === 4)[0];
-  t.equal(stream.status, 200, "# should get the data successfully");
-  t.equal(zeroesVersion.version, "v0.12.17", "# v0 version should match");
-  t.equal(forthVersion.version, "v4.9.0", "# v4 version should match");
+  t.equal(stream.status, 200, "* should get the data successfully");
+  t.equal(zeroesVersion.version, "v0.12.17", "* v0 version should match");
+  t.equal(forthVersion.version, "v4.9.0", "* v4 version should match");
 });
 
-tape("should get latest-releases", async (t) => {
+tape("/latest-releases - should get latest-releases", async (t) => {
   const getStream = bent(context.origin);
   const stream = await getStream("/latest-releases");
   t.plan(3);
 
   const versions = await stream.json();
   const thirteenthVersion = versions.filter(
-    (v) => semver.major(v.version) === 13
+    (v) => semver.major(v.version) === 12
   )[0];
   const fourteenthVersion = versions.filter(
-    (v) => semver.major(v.version) === 14
+    (v) => semver.major(v.version) === 13
   )[0];
-  t.equal(stream.status, 200, "# should get the data successfully");
-  t.equal(thirteenthVersion.version, "v13.14.0", "# v13 version should match");
-  t.equal(fourteenthVersion.version, "v14.10.1", "# v14 version should match");
+  t.equal(stream.status, 200, "* should get data successfully");
+  t.equal(thirteenthVersion.version, "v12.22.1", "* v12 version should match");
+  t.equal(fourteenthVersion.version, "v13.14.0", "* v13 version should match");
 });
 
 tape("teardown", (t) => {
